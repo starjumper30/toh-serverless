2b70f7157b3391cc4d831640a20e7deaf3c9059f
ChrisHardin <chris.hardin@oasisdigital.com>
2019-01-22 16:45:08 -0600

    Added firebase authentication

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+import { environment } from "../environments/environment";
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
@@ -17,6 +21,8 @@ import { MessagesComponent }    from './messages/messages.component';
 
 @NgModule({
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
     BrowserModule,
     FormsModule,
     AppRoutingModule,
